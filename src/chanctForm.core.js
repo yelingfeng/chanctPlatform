52fa00de3b6dc1121ec53aaa3c4084f339480531
renxiaofan <yelingfeng521@gmail.com>
2017-07-22 15:30:48 +0800

    add bili and xo

---
+++ src/chanctForm.core.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ylf on 2017/7/22.
+ */
+
+export default class chanctForm {
+  constructor () {
+    this.init()
+  }
+
+  init () {
+    this.form = {}
+  }
+
+  getForm () {
+    return this.form
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ylf on 2017/7/22.
+ */
+import ChanctForm from './chanctForm.core'
+
+export const initForm = () => {
+  const cf = new ChanctForm()
+  console.log(cf.getForm())
+}
+
