365f314713f3192aaef7befbcb4fa4a9bf33230d
renxiaofan <yelingfeng521@gmail.com>
2017-09-17 16:45:46 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -3,11 +3,11 @@
  */
 import Vue from 'vue'
 import App from './App'
-import ChanctForm from '../src/index'
+import xForm from '../src/index'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css';
 import '../src/style/index.css'
-Vue.use(ChanctForm)
+Vue.use(xForm)
 Vue.use(elementUI)
 new Vue({
   el: '#app',
